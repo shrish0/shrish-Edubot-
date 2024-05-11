ee2aa8b45035a18be6a6465992ae27e035e12587
shrish0 <shrishgupta1234@gmail.com>
2024-05-11 13:33:02 +0530

    add some blog model

--- pages/models.py
+++ pages/models.py
@@ -13,7 +13,7 @@ class ContactData(models.Model):
         return self.name + " - " + self.email
     
 class CourseDetail(models.Model):
-    className=models.CharField(max_length=50)
+    className=models.CharField(max_length=50,default="")
     courseName = models.CharField(max_length=100)
     chapter = models.CharField(max_length=100)
     video = models.TextField()
@@ -22,3 +22,11 @@ class CourseDetail(models.Model):
 
     def __str__(self):
         return self.className + " - " + self.courseName
+    
+class Blogs(models.Model):
+    topic=models.CharField(max_length=50)
+    link=models.TextField()
+    subject=models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return self.topic + " " + self.created_at
